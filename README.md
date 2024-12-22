3b4f67ce059d8cb3c2023e1dcaf9f088f5911fc3
Sachin Bhone <sachinbhone40@gmail.com>
2024-12-22 13:42:55 +0530

    C-1 Project setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# netflix gpt
+
+- create react app
+- configured TailwindCSS
---
+++ src/App.js
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div className="font-bold text-center text-3xl">Hello</div>;
+}
+
+export default App;
